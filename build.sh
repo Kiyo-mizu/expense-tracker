5a22e4bc840902c3689c6b14532c1c99698a1fbb
C.J. Manahan <manahancj16@gmail.com>
2025-08-04 19:55:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Set Python path to include the current directory
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+
 cd expense_tracker
 python manage.py collectstatic --no-input
 python manage.py migrate 
