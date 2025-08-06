622a2bef0e00b04c7eaa4de546dfdd8acd77af94
C.J. Manahan <manahancj16@gmail.com>
2025-08-06 09:55:56 +0800

    Update build.sh

--- expense_tracker/build.sh
+++ expense_tracker/build.sh
@@ -7,6 +7,5 @@ pip install -r requirements.txt
 # Set Python path to include the current directory
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
-cd expense_tracker
 python manage.py collectstatic --no-input
 python manage.py migrate 
